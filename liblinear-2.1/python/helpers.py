367eaeaf47ddb39a1e63877aad3e3b9dd68a5673
Andrew Aday <andrewladay@gmail.com>
2015-11-07 16:42:24 -0500

    added test for polluted initial detection accuracy

--- liblinear-2.1/python/helpers.py
+++ liblinear-2.1/python/helpers.py
@@ -1,10 +1,10 @@
 
-def seconds_to_english(seconds):
-    seconds_trunc_1 = int(seconds // 60) * 60
-    s = seconds - seconds_trunc_1
-    seconds -= s
-    seconds /= 60
-    seconds_trunc_2 = int(seconds // 60) * 60
-    m = int(seconds - seconds_trunc_2)
-    h = seconds_trunc_2 / 60
+def sec_to_english(sec):
+    sec_trunc_1 = int(sec // 60) * 60
+    s = sec - sec_trunc_1
+    sec -= s
+    sec /= 60
+    sec_trunc_2 = int(sec // 60) * 60
+    m = int(sec - sec_trunc_2)
+    h = sec_trunc_2 / 60
     return str(h) + " hours, " + str(m) + " minutes, and " + str(s) + " seconds."
